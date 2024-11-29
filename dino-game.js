d773cb732be431c695970654e34df8d89d157717
Antoluc15 <144289430+Antoluc15@users.noreply.github.com>
2024-11-29 16:47:23 -0600

    Update dino-game.js

--- dino-game.js
+++ dino-game.js
@@ -84,8 +84,11 @@ window.addEventListener('load', () => {
             ctx.font = '20px Arial';
             ctx.fillText(`Puntaje final: ${score}`, canvas.width / 2 - 70, canvas.height / 2 + 40);
             ctx.fillText('Presiona "Reiniciar" para jugar de nuevo', canvas.width / 2 - 150, canvas.height / 2 + 70);
+
             restartGameButton.style.display = 'block'; // Mostrar el botón de reinicio
             restartGameButton.style.zIndex = '10'; // Asegúrate de que esté en frente
+            restartGameButton.style.position = 'absolute'; // Aseguramos que esté en una capa superior
+            restartGameButton.style.top = `${canvas.height / 2 + 100}px`; // Ajustamos la posición del botón
         };
 
         // Actualizar los obstáculos
