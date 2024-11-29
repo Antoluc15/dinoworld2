7c8f56f6bbd3891492040cd2380ccc7756112f32
Antoluc15 <144289430+Antoluc15@users.noreply.github.com>
2024-11-29 16:14:25 -0600

    Update dino-game.js

--- dino-game.js
+++ dino-game.js
@@ -76,7 +76,9 @@ window.addEventListener('load', () => {
             ctx.font = '20px Arial';
             ctx.fillText('Presiona "Reiniciar" para jugar de nuevo', canvas.width / 2 - 150, canvas.height / 2 + 40);
             restartGameButton.style.display = 'block'; // Mostrar el botón de reinicio
+            restartGameButton.style.zIndex = '10'; // Asegúrate de que esté en frente
         };
+
 
         // Actualizar los obstáculos
         const updateObstacles = () => {
