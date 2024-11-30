706eafd3ee8a41ecd38c446fefb45c5b8827b0db
Antoluc15 <144289430+Antoluc15@users.noreply.github.com>
2024-11-29 23:25:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -29,16 +29,17 @@ pool.connect((err) => {
 });
 
 // Ruta para obtener el récord global
-app.get('/api/highscore', async (req, res) => {
+app.post('/api/highscore', async (req, res) => {
+  const { score } = req.body;
   try {
-    const result = await pool.query('SELECT score FROM highscore ORDER BY score DESC LIMIT 1');
-    const highScore = result.rows[0] ? result.rows[0].score : 0;
-    res.json({ highScore });
+    await pool.query('INSERT INTO highscore (score) VALUES ($1)', [score]);
+    res.status(201).json({ message: 'High score updated' });
   } catch (err) {
-    console.error('Error fetching high score:', err);
-    res.status(500).json({ error: 'Error fetching high score' });
+    console.error('Error updating high score:', err);
+    res.status(500).json({ error: 'Error updating high score' });
   }
 });
+
 
 // Ruta para actualizar el récord global
 app.post('/api/highscore', async (req, res) => {
